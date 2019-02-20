e23f68b37c5ae8a6309e570d0010c0da0c37f5eb
fbeji <fbeji@unm.edu>
2019-02-19 21:31:32 -0700

    Initial commit check-digits

---
+++ src/edu/cnm/deepdive/Check.java
@@ -0,0 +1,23 @@
+package edu.cnm.deepdive;
+
+public class Check {
+
+  public static void main(String[] args) {
+    int val = 0 ;
+    digitSum(val);
+    System.out.print(val);
+  }
+
+  public static int digitSum(int param) {
+
+    String var = Integer.toString(param);
+
+    int param1 = 0;
+
+    for (int i = 0; i < var.length(); i++) {
+      char c = var.charAt(i);
+       param1 =param1 + Character.getNumericValue(c); }
+
+
+  return param1;
+}}
