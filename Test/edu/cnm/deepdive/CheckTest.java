c3c3c5c0b49a99a1dc3f8fabfd1454b2370c2d32
fbeji <fbeji@unm.edu>
2019-02-20 14:00:38 -0700

    check-digits class

--- Test/edu/cnm/deepdive/CheckTest.java
+++ Test/edu/cnm/deepdive/CheckTest.java
@@ -1,28 +1,88 @@
 package edu.cnm.deepdive;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Random;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 class CheckTest {
 
-  @Test
-  void main() {
+  @ParameterizedTest(name = "[{index}] input = {0}; expected = {1}")
+  @CsvFileSource(resources = "test.csv")
+  @DisplayName("Check sum of digits")
+  
+  void digitSum(int input, int expected) {
 
-
+      int actual = Check.digitSum(input);
+      assertEquals(expected, actual);
+    }
   }
 
-  @Test
-  void digitSum() {
-    assertEquals(4046, 14);
-    assertEquals(6325, 16);
-    assertEquals(9788, 32);
-    assertEquals(5676, 24);
-    assertEquals(88, 16);
-    assertEquals(8925, 24);
-    assertEquals(8598, 30);
-    assertEquals(2025, 9);
-    assertEquals(4738, 22);
-    assertEquals(453, 12);
-  }
-}
+
+
+//  static final int[] INPUT = {
+//      4046,
+//      6325,
+//      9788,
+//      5676,
+//      88,
+//      8925,
+//      8598,
+//      2025,
+//      4738,
+//      453
+//  };
+//  static final int[] EXPECTED = {
+//      14,
+//      16,
+//      32,
+//      24,
+//      16,
+//      24,
+//      30,
+//      9,
+//      22,
+//      12
+//  };
+//
+//  @Test
+//  void digitSum() {
+//    for (int i = 0; i < INPUT.length; i++) {
+//      int input = INPUT[i];
+//      int expected = EXPECTED[i];
+//      int actual = Check.digitSum(input);
+//      assertEquals(expected, actual);
+//    }
+//  }
+
+//}
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import org.junit.jupiter.api.Test;
+//
+//class CheckTest {
+//
+//  @Test
+//  void main() {
+//
+//
+//  }
+//
+//  @Test
+//  void digitSum() {
+//    assertEquals(4046, 14);
+//    assertEquals(6325, 16);
+//    assertEquals(9788, 32);
+//    assertEquals(5676, 24);
+//    assertEquals(88, 16);
+//    assertEquals(8925, 24);
+//    assertEquals(8598, 30);
+//    assertEquals(2025, 9);
+//    assertEquals(4738, 22);
+//    assertEquals(453, 12;
+//  }
+//}
--- src/edu/cnm/deepdive/Check.java
+++ src/edu/cnm/deepdive/Check.java
@@ -1,23 +1,49 @@
 package edu.cnm.deepdive;
 
+
 public class Check {
 
-  public static void main(String[] args) {
-    int val =0 ;
-    digitSum(val);
-    System.out.print(val);
+  private static final String OUTPUT_FORMAT =
+      "Input = %d; remainder = %d; digit sum = %d; remainder = %d.%n";
+  private static final int MODULUS = 9;
+
+  public static int digitSum(int input) {
+    int sum = 0;
+    String inStr = "" + input;
+    for (char c : inStr.toCharArray()) {
+      sum += c - '0';
+    }
+    return sum;
   }
 
-  public static int digitSum(int param) {
+  public static void main(String[] args) {
+    int input = Integer.parseInt(args[0]);
+    int sum = digitSum(input);
+    System.out.printf(OUTPUT_FORMAT, input, input % MODULUS, sum, sum % MODULUS);
+  }
 
-    String var = Integer.toString(param);
-
-    int param1 = 0;
-
-    for (int i = 0; i < var.length(); i++) {
-      char c = var.charAt(i);
-       param1 =param1 + Character.getNumericValue(c); }
+}
 
 
-  return param1;
-}}
+
+//public class Check {
+//
+//  public static void main(String[] args) {
+//    int val = 0 ;
+//    digitSum(val);
+//    System.out.print(val);
+//  }
+//
+//  public static int digitSum(int param) {
+//
+//    String var = Integer.toString(param);
+//
+//    int param1 = 0;
+//
+//    for (int i = 0; i < var.length(); i++) {
+//      char c = var.charAt(i);
+//       param1 =param1 + Character.getNumericValue(c); }
+//
+//
+//  return param1;
+//}}
